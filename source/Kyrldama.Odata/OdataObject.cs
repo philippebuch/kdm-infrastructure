a2364b4fe22b9413bea8be006a633b2e2a435a0e
Philippe Buch <gabrielfanel@gmail.com>
2023-06-22 08:38:41 +0200

    feat: init

---
+++ source/Kyrldama.Odata/OdataObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Kyrldama.Odata
+{
+    public class OdataObject
+    {
+        private Dictionary<string, JsonProperty> Properties = new Dictionary<string, JsonProperty>();
+
+        protected void Set<T>(string propertyName, T value)
+        {
+            JsonProperty property;
+
+            if (!Properties.ContainsKey(propertyName))
+                property = Properties[propertyName];
+        }
+    }
+}
